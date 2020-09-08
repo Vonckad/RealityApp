1e42f59c34ba0e208af4867e4c77d5ec0a1a11d0
Vonckad <Vonkad@yandex.ru>
2020-09-08 23:16:49 +0300

    arView.session.run(configuration) нет такого метода

--- RealityApp/ViewController.swift
+++ RealityApp/ViewController.swift
@@ -5,20 +5,26 @@
 //  Created by Vlad Ralovich on 9/8/20.
 //
 
-import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    
+//    @IBOutlet var arScnView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let configuration = ARFaceTrackingConfiguration()
+        
+//        arScnView.session.run(configuration)
+        arView.session.run(configuration)
+
+        
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let faceAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(faceAnchor)
     }
 }
