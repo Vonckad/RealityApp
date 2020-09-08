2af0642816f7505a69c69b53b02f4e22c53cd232
Vonckad <Vonkad@yandex.ru>
2020-09-08 22:19:23 +0300

    Initial Commit

---
+++ RealityApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RealityApp
+//
+//  Created by Vlad Ralovich on 9/8/20.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
